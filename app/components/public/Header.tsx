8e7709c6d6c915d19b3901ec738a58ee8032ffd8
TrungTT <trung.tran1@ntq-solution.com.vn>
2026-01-25 11:00:15 +0700

    update t register

--- app/components/public/Header.tsx
+++ app/components/public/Header.tsx
@@ -105,7 +105,7 @@ export default async function Header() {
                 {t("menu.login")}
               </a>
               <a href="/register" className="btn btn-register">
-                {t("menu.register")}
+                {t("page.register")}
               </a>
             </div>
           </div>
