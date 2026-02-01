a492e4914271bb0188a80589f6ff88ed9926dbfb
TrungTT <trung.tran1@ntq-solution.com.vn>
2026-02-01 21:53:23 +0700

    set user agent

--- app/app/[locale]/(auth)/verify-otp/action.ts
+++ app/app/[locale]/(auth)/verify-otp/action.ts
@@ -1,18 +1,27 @@
 'use server';
 
 import { redirect } from 'next/navigation';
-import { cookies } from 'next/headers';
 import {resendOtp, verifyOtp} from "@/services/api/public/auth";
 import {cookieUtil} from "@/lib/cookieUtil";
+import { headers as nextHeaders } from 'next/headers';
 export async function verifyOtpAction(formData: FormData) {
-  const cookieStore = await cookies();
   const numbers = formData.getAll('numbers[]') as string[];
   const otp = numbers.join('');
   const payload = {
     email: formData.get('email')?.toString() ?? '',
     otp: otp
   };
-  const res = await verifyOtp(payload)
+
+  const h = await nextHeaders();
+  const headers = {
+    'Content-Type': 'application/json',
+    'user-agent': h.get('user-agent') ?? '',
+    'x-forwarded-for':
+      h.get('x-forwarded-for')?.split(',')[0] ??
+      h.get('x-real-ip') ??
+      '',
+  };
+  const res = await verifyOtp(payload, headers)
 
   if (!res.ok) {
     await cookieUtil.set('verify-otp-message', {
--- app/app/[locale]/verify/route.ts
+++ app/app/[locale]/verify/route.ts
@@ -1,34 +1,37 @@
 
 import { NextRequest, NextResponse } from 'next/server';
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8088/api';
+import {headers as nextHeaders} from "next/dist/server/request/headers";
+import {verify} from "@/services/api/public/auth";
+import {cookieUtil} from "@/lib/cookieUtil";
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3100';
 export async function GET(req: NextRequest) {
   const token = req.nextUrl.searchParams.get('token');
   if (!token) {
     return NextResponse.redirect(new URL(`${FRONTEND_URL}/login`));
   }
+  const h = await nextHeaders();
+  const headers = {
+    'Content-Type': 'application/json',
+    'user-agent': h.get('user-agent') ?? '',
+    'x-forwarded-for':
+      h.get('x-forwarded-for')?.split(',')[0] ??
+      h.get('x-real-ip') ??
+      '',
+  };
+  const payload = {
+    token: token
+  }
   try {
-    const res = await fetch(`${API_BASE_URL}/auth/verify`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        token: token
-      }),
-      cache: 'no-store',
-    });
+    const res = await verify(payload, headers)
 
     if (res.ok) {
       const result: any = await res.json()
       const accessToken = result?.data?.accessToken;
       const response = NextResponse.redirect(new URL(`${FRONTEND_URL}/user/profile`));
-      response.cookies.set({
-        name: 'token',
-        value: accessToken,
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        maxAge: 60 * 60 * 24
-      });
+      await cookieUtil.set('token', accessToken, {
+        httpOnly: false,
+        ttl: 7 * 24 * 60 * 60 // 7 days
+      })
 
       return response;
     }
