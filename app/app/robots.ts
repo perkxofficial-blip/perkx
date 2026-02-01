7718d4ad66bbc8644ce499907adfb73bc0c2c1f9
HuyDV <huydv>
2026-02-01 17:18:43 +0700

    [Add] robots.txt

--- app/app/robots.ts
+++ app/app/robots.ts
@@ -1,23 +1,4 @@
 import type { MetadataRoute } from 'next'
- 
-type Robots = {
-  rules:
-    | {
-        userAgent?: string | string[]
-        allow?: string | string[]
-        disallow?: string | string[]
-        crawlDelay?: number
-      }
-    | Array<{
-        userAgent: string | string[]
-        allow?: string | string[]
-        disallow?: string | string[]
-        crawlDelay?: number
-      }>
-  sitemap?: string | string[]
-  host?: string
-}
-
 
 export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.FRONTEND_URL
