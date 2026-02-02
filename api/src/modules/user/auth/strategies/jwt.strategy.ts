c5e73895c8fe1892f4120e64d99b0f01a755cb2d
QuanLuu <quan.luu1@ntq-solution.com.vn>
2026-02-02 21:50:55 +0700

    [Modify] Logout user when status is deactivate

--- api/src/modules/user/auth/strategies/jwt.strategy.ts
+++ api/src/modules/user/auth/strategies/jwt.strategy.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User, UserStatus } from '../../../../entities';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(private configService: ConfigService) {
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,6 +21,15 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   }
 
   async validate(payload: any) {
-    return { id: payload.sub, email: payload.email };
+    const user = await this.userRepository.findOne({
+      where: { id: payload.sub },
+      select: ['id', 'email', 'status'],
+    });
+    if (user.status === UserStatus.DEACTIVATE) {
+      throw new UnauthorizedException('Account has been deactivated');
+    }
+
+    // Return user info for request.user
+    return { id: user.id, email: user.email };
   }
 }
