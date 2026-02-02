afa8ed01ce0504632c93f49f22d4ed7013c22641
TrungTT <trung.tran1@ntq-solution.com.vn>
2026-02-02 16:10:01 +0700

    [Fix] error verify

--- app/app/[locale]/verify/route.ts
+++ app/app/[locale]/verify/route.ts
@@ -1,41 +1,48 @@
-
 import { NextRequest, NextResponse } from 'next/server';
-import {headers as nextHeaders} from "next/dist/server/request/headers";
-import {verify} from "@/services/api/public/auth";
-import {cookieUtil} from "@/lib/cookieUtil";
+import { headers } from 'next/headers';
+import { verify } from '@/services/api/public/auth';
+import { cookieUtil } from '@/lib/cookieUtil';
+
 const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3100';
+
 export async function GET(req: NextRequest) {
   const token = req.nextUrl.searchParams.get('token');
+
   if (!token) {
     return NextResponse.redirect(new URL(`${FRONTEND_URL}/login`));
   }
-  const h = await nextHeaders();
-  const headers = {
-    'Content-Type': 'application/json',
-    'user-agent': h.get('user-agent') ?? '',
-    'x-forwarded-for':
-      h.get('x-forwarded-for')?.split(',')[0] ??
-      h.get('x-real-ip') ??
-      '',
-  };
-  const payload = {
-    token: token
-  }
+
   try {
-    const res = await verify(payload, headers)
+    const h = await headers();
 
-    if (res.ok) {
-      const result: any = await res.json()
-      const accessToken = result?.data?.accessToken;
-      const response = NextResponse.redirect(new URL(`${FRONTEND_URL}/user/profile`));
-      await cookieUtil.set('token', accessToken, {
-        httpOnly: false,
-        ttl: 7 * 24 * 60 * 60 // 7 days
-      })
+    const apiHeaders = {
+      'Content-Type': 'application/json',
+      'user-agent': h.get('user-agent') ?? '',
+      'x-forwarded-for':
+        h.get('x-forwarded-for')?.split(',')[0] ??
+        h.get('x-real-ip') ??
+        '',
+    };
 
-      return response;
+    const res = await verify({ token }, apiHeaders);
+    const result: any = await res.json();
+    if (!res.ok) {
+      await cookieUtil.set('login-message', {
+        status: false,
+        message: result?.message ?? 'message.token_invalid',
+      }, {ttl: 10})
+      return NextResponse.redirect(new URL(`${FRONTEND_URL}/login`));
     }
+
+    const accessToken = result?.data?.accessToken;
+    await cookieUtil.set('token', accessToken, {
+      httpOnly: false,
+      ttl: 7 * 24 * 60 * 60,
+    });
+
+    return NextResponse.redirect(new URL(`${FRONTEND_URL}/user/profile`));
   } catch (err) {
+    console.error('Verify error:', err);
     return NextResponse.redirect(new URL(`${FRONTEND_URL}/login`));
   }
 }
