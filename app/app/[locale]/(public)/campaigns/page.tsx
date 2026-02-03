541bf50062f25c4ba3bbc585ec2203b64370fab0
TrungTT <trung.tran1@ntq-solution.com.vn>
2026-02-03 09:01:22 +0700

    Update page.tsx

--- app/app/[locale]/(public)/campaigns/page.tsx
+++ app/app/[locale]/(public)/campaigns/page.tsx
@@ -55,8 +55,8 @@ export default async function CampaignPage({ searchParams }: Props) {
   const t = await getTranslations();
   const categories = [
     {value:'', label: t('campaign.all_category')},
+    {value:'all_user', label: t('campaign.category_all_user')},
     {value:'new_user', label: t('campaign.category_new_user')},
-    // {value:'trading_competition ', label: t('campaign.category_trading_competition')},
   ]
   const statuses = [
     {value:'', label: t('campaign.all_campaign_status')},
