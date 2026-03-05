3efe4536003bcb692b31e801647f0b74b436499d
TrungTT <trung.tran1@ntq-solution.com.vn>
2026-03-05 16:59:50 +0700

    css

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -30,12 +30,12 @@ import { MailModule } from './mail/mail.module';
         database: configService.get<string>('database.database'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: false,
-        ssl: process.env.NODE_ENV === 'production' ? true : false, // Enable SSL for secure connection to AWS RDS
-        extra: {
-          ssl: {
-            rejectUnauthorized: process.env.NODE_ENV === 'production' ? false : true,
-          },
-        },
+        // ssl: process.env.NODE_ENV === 'production' ? true : false, // Enable SSL for secure connection to AWS RDS
+        // extra: {
+        //   ssl: {
+        //     rejectUnauthorized: process.env.NODE_ENV === 'production' ? false : true,
+        //   },
+        // },
       }),
       inject: [ConfigService],
     }),
